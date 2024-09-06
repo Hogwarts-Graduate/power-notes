9ee5549713487acb61e2fd82905364f64e743c57
A Hogwarts' Ph.D <ywtflying@outlook.com>
2024-09-06 21:37:55 +0800

    add base path in config.mjs
    add base path in config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/power-notes/",
   title: "power-notes",
   description: "a notes website",
   themeConfig: {
